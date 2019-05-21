d343c785a27d9ac1870c97f61149c8162e229252
Trish Ogen <patriciaogen@gmail.com>
2019-05-21 02:58:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,6 +31,8 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  monopoly[:railroads][:names][:reading_railroad] = {}
+
   monopoly
 end
 
